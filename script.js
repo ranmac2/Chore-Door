53df0d317bf7614f4d398e94e548d49ea3410d03
Randy <randy_maclean@hotmail.ca>
2021-04-26 16:01:55 -0700

    Added door row and padding

---
+++ script.js
@@ -0,0 +1,5 @@
+const doorImage1 = document.getElementById('door1');
+let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+doorImage1.onclick = () => {
+    doorImage1.src = botDoorPath;
+};
