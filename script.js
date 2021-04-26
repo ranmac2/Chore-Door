8a9dff519c455a923315c81074c20d02b73b5b2b
Randy <randy_maclean@hotmail.ca>
2021-04-26 16:05:39 -0700

    Added beach door and space door variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const doorImage1 = document.getElementById('door1');
-let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+const doorImage2 = document.getElementById('door2');
+const doorImage3 = document.getElementById('door3');
+
+const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
+};
+
+doorImage2.onclick = () => {
+    doorImage2.src = beachDoorPath;
+};
+
+doorImage3.onclick = () => {
+    doorImage3.src = spaceDoorPath;
 };
